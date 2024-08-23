4eea0fa96759ece4fb5720e5b591b3efa5207403
Vinay Rajput <vnaykrajput@gmail.com>
2024-08-23 21:53:18 +0530

    Initial commit

---
+++ src/main/java/com/masapp/medicaladminsystem/MedicalAdminSystemApplication.java
@@ -0,0 +1,13 @@
+package com.masapp.medicaladminsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAdminSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalAdminSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/masapp/medicaladminsystem/MedicalAdminSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masapp.medicaladminsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAdminSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
